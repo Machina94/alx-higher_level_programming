d359c3c72027ab32ea6fdbcfc999ed9c9c1fb7cb
Machina94 <chinedumaduka94@yahoo.com>
2022-06-10 20:07:03 -0700

    task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        return [list(map(lambda j: j**2, i)) for i in matrix]
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
